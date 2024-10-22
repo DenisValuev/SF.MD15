625e362ae13989bd3281bbabdee52fdf94fcdee1
DenisValuev <valuev.denis91@mail.ru>
2024-10-22 12:16:36 +0300

    Добавьте файлы проекта.

---
+++ SF.MD15.U1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SF.MD15.U1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
